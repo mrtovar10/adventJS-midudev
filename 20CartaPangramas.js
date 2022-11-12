806f88644dc0dac6c0b300cad538f5008de64804
mrtovar10 <mrtovar10@gmail.com>
2022-11-12 15:55:13 -0400

    20 resuelto

---
+++ 20CartaPangramas.js
@@ -0,0 +1,25 @@
+// En la clase de español del pueblo de Laponia han creado un reto a la hora de escribir la carta a Papa Noél 🎅: la carta ✉️ tiene que contener todas las letras del alfabeto.
+
+// Desde el taller de Santa 🎅 se han enterado y quieren escribir una función que les diga si realmente la cadena de texto que les llega tiene, efectivamente, todas las letras del abecedario español 🔎.
+
+// Hay que tener en cuenta las letras en mayúscula y que las letras con acento y diéresis se consideran iguales. Por ejemplo la á y la ä cuenta como una a.
+
+// Vamos a ver unos ejemplos de frases:
+
+// pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho') // true
+// pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!') // true
+
+// pangram('Esto es una frase larga pero no tiene todas las letras del abecedario') // false
+// pangram('De la a a la z, nos faltan letras') // false
+// Y ya que estás... ¿Cuál es tu pangrama favorito? ¡Compártelo en nuestra comunidad de Discord!
+
+// Elimina los diacríticos de un texto excepto si es una "ñ" (ES6)
+
+export default function pangram(letter) {
+    // ¡No olvides compartir tu solución en redes!
+    let arr = [... new Set(letter.split(' ').join('').toLowerCase().split(''))]
+    let compare = 'abcdefghijklmnopqrstuvwxyzñ'.split('')
+    let arrFilteredSorted = arr.filter(value => compare.includes(value)).sort()
+    let texto = arrFilteredSorted.join('')
+    return texto === 'abcdefghijklmnopqrstuvwxyzñ'
+}
